42e0a46adc22e3a57b2724fc86ccb8c40539f1d8
MikeH <michael.h.humphrey@gmail.com>
2015-02-03 15:13:48 -0500

    added css to show and no longer require image

--- app/models/door.rb
+++ app/models/door.rb
@@ -3,7 +3,7 @@ class Door < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
-  validates :image, presence: true
+  #validates :image, presence: true
   validates :name, presence: true
 
 	measure_from = [['Door', 1], ['Frame', 2]] 
